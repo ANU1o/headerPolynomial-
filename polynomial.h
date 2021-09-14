e6c908d51621ad15805fb23f0f6ab0c023a72331
ANU 10 <78339400+ANU1o@users.noreply.github.com>
2021-09-14 10:34:45 +0300

    Update polynomial.h

--- polynomial.h
+++ polynomial.h
@@ -6,10 +6,8 @@ using namespace std;
 class Polynomial
 {
 
-    int x;
-
     public:
-        int a[50];
+        int a[50], x;
 
         int limit(int n)    //intialize limit for degree of equation using this
         {
@@ -27,7 +25,7 @@ class Polynomial
             
         }
 
-        void read()
+        void read()     //read constant to equation
         {
 
             for(int i = 0; i < x; i++)
@@ -40,7 +38,7 @@ class Polynomial
 
         }
 
-        void display()
+        void display()      // display constants from equation corresponding to the degree of variable
         {
 
             cout << "\nPolynomial\n";
@@ -52,6 +50,40 @@ class Polynomial
 
             }
 
+        }
+
+        int binarySearch(int l, int r, int x)       //searches constant at assigned degree
+        {
+
+            if (r >= l)
+            {
+                int mid = l + (r - l) / 2;
+                
+                // If the element is present at the middle
+                // itself
+                if (a[mid] == x)
+                    return mid;
+
+                // If element is smaller than mid, then
+                // it can only be present in left subarray
+                if (a[mid] > x)
+                    return binarySearch(l, mid - 1, x);
+                
+
+            
+
+                // Else the element can only be present
+                // in right subarray
+                
+                return binarySearch(mid + 1, r, x);
+                
+
+            }
+
+
+            // We reach here when element is not
+            // present in array
+            return -1;
         }
 
 };
